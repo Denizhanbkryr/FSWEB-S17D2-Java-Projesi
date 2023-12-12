1e4e147987a7cefbd5df88e0a4e9f71b56a66455
Denizhan Bakıryor <denizhanbakiryor@gmail.com>
2023-12-12 21:32:56 +0300

    S17D2 Project solved.

---
+++ src/main/java/com/workintech/s17d2/tax/DeveloperTax.java
@@ -0,0 +1,23 @@
+package com.workintech.s17d2.tax;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeveloperTax implements Taxable{
+
+
+    @Override
+    public Double getSimpleTaxRate() {
+       return 0.1;
+    }
+
+    @Override
+    public Double getMiddleTaxRate() {
+        return 0.2;
+    }
+    @Override
+    public Double getUpperTaxRate() {
+        return 0.3;
+
+    }
+}
